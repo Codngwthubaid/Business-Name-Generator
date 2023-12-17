285ccc4b8daf192f412bca6327631225fa2c4ec8
UltraEdit-Clone <codngwthubaid@gmail.com>
2023-12-17 15:11:31 +0530

    Add readme file and write complete code of business name generator program

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Repository -
+
+This code is written for the a program which generates a random Business Name for the user.
+Programmer = Mohammad Ubaid Ansari
---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Mohammad Ubaid Asnari are ready to create the Business Name Generator Exrecise -")
+
+const BN = ("Welcome to Business Name Generator")
+console.log(BN)
+
+let Adjectives = {
+    adj1 : "Crazy",
+    adj2 : "Amazing",
+    adj3 : "Fire"
+}
+
+let Shop_Name = {
+    SN1 : "Enginne",
+    SN2 : "Food",
+    SN3 : "Garments"
+}
+
+let Another_Name = {
+    AN1 : "Bros",
+    AN2 : "Limited",
+    AN3 : "Hub"
+}
+
+let random = Math.random()
+if (0.2<= random <= 0.4){
+    console.log(random)
+    console.log(`${adj1} ${SN2} ${AN3}`)
+}
+    
+
